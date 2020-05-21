ac8ddd333592fea8a1d8f466f6082e96305f7ccd
nehc9050 <chendavid2012@gmail.com>
2020-05-21 18:15:06 -0400

    mess around with express

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Navbar variant="dark">
+        <Navbar.Brand>hts.directory</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
